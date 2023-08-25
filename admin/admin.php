c15092f94be7bf366fb855976ba63fae5866306a
Innama Asghar <111514134+asghar-innama@users.noreply.github.com>
2023-08-25 11:26:59 +0530

    init

---
+++ admin/admin.php
@@ -0,0 +1,151 @@
+
+<?php
+ob_start(); 
+// $connection = mysqli_connect("localhost:3307", "root", "");
+// $db = mysqli_select_db($connection, 'demo');
+ include("connect.php"); 
+if($_SESSION['name']==''){
+	header("location:signin.php");
+}
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>Admin Dashboard Panel</title> 
+    
+<?php
+ $connection=mysqli_connect("localhost:3307","root","");
+ $db=mysqli_select_db($connection,'demo');
+ 
+
+
+?>
+</head>
+<body>
+    <nav>
+        <div class="logo-name">
+            <span class="logo_name">ADMIN</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dahsboard</span>
+                </a></li>
+                <!-- <li><a href="#">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Content</span>
+                </a></li> 
+                <li><a href="analytics.php">
+                    <i class="uil uil-chart"></i>
+                    <span class="link-name">Analytics</span>
+                </a></li>-->
+                <li><a href="donate.php">
+                    <i class="uil uil-heart"></i>
+                    <span class="link-name">Donates</span>
+                </a></li>
+                <li><a href="feedback.php">
+                    <i class="uil uil-comments"></i>
+                    <span class="link-name">Feedbacks</span>
+                </a></li>
+                <li><a href="adminprofile.php">
+                    <i class="uil uil-user"></i>
+                    <span class="link-name">Profile</span>
+                </a></li>
+                <!-- <li><a href="#">
+                    <i class="uil uil-share"></i>
+                    <span class="link-name">Share</span>
+                </a></li> -->
+            </ul>
+            
+            <ul class="logout-mode">
+                <li><a href="../logout.php">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+            <!-- <p>Food Donate</p> -->
+            <p  class ="logo" >Sustainable <b style="color: #06C167; ">Bites</b></p>
+             <p class="user"></p>
+            <!-- <div class="search-box">
+                <i class="uil uil-search"></i>
+                <input type="text" placeholder="Search here...">
+            </div> -->
+            
+            <!--<img src="images/profile.jpg" alt="">-->
+        </div>
+
+        <div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                    <i class="uil uil-tachometer-fast-alt"></i>
+                    <span class="text">Dashboard</span>
+                </div>
+
+                <div class="boxes">
+                    <div class="box box1">
+                        <i class="uil uil-user"></i>
+                        <!-- <i class="fa-solid fa-user"></i> -->
+                        <span class="text">Total users</span>
+                        <!--?php
+                           $query="SELECT count(*) as count FROM  login";
+                           $result=mysqli_query($connection, $query);
+                           $row=mysqli_fetch_assoc($result);
+                         echo "<span class=\"number\">".$row['count']."</span>";
+                        ?>
+                         <span class="number">50,120</span> -->
+                    </div>
+                    <div class="box box2">
+                        <i class="uil uil-comments"></i>
+                        <span class="text">Feedbacks</span>
+                         <!--?php
+                           $query="SELECT count(*) as count FROM  user_feedback";
+                           $result=mysqli_query($connection, $query);
+                           $row=mysqli_fetch_assoc($result);
+                         echo "<span class=\"number\">".$row['count']."</span>";
+                        ?>
+                         <span class="number">20,120</span> -->
+                    </div>
+                    <div class="box box3">
+                        <i class="uil uil-heart"></i>
+                        <span class="text">Total doantes</span>
+                         <!--?php
+                           $query="SELECT count(*) as count FROM food_donations";
+                           $result=mysqli_query($connection, $query);
+                           $row=mysqli_fetch_assoc($result);
+                         echo "<span class=\"number\">".$row['count']."</span>";
+                        ?>
+                        <span class="number">10,120</span> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="activity">
+                <div class="title">
+                    <i class="uil uil-clock-three"></i>
+                    <span class="text">Recent Donations</span>
+                </div>
+            <div class="get">
+            
+
+    <script src="admin.js"></script>
+</body>
+</html>
---
+++ connection.php
@@ -0,0 +1,7 @@
+
+
+<?php
+//change mysqli_connect(host_name,username, password); 
+$connection = mysqli_connect("localhost:3307", "root", "");
+$db = mysqli_select_db($connection, 'demo');
+?>
